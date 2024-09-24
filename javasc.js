640d08ff40f6a64a0968a234d8f0ae1b03fdd433
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-09-24 17:00:03 +0530

    Update javasc.js

--- javasc.js
+++ javasc.js
@@ -83,5 +83,5 @@ function mod1(){
     var n1= document.getElementById("n1").value;
     var n2= document.getElementById("n2").value;
     var mod = n1%n2;
-    document.getElementById("result").innerHTML=div
+    document.getElementById("result").innerHTML=mod
 }
